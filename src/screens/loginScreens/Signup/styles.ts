1c1069341d9827ddfd86696477e9ad4eb5b38f2d
Kwasi Fraser <followthescript@gmail.com>
2023-07-14 17:33:18 -0400

    TOSand18andupinvoked

--- src/screens/loginScreens/Signup/styles.ts
+++ src/screens/loginScreens/Signup/styles.ts
@@ -2,6 +2,41 @@ import {StyleSheet} from 'react-native';
 import {COLORS, SIZES, FONTS} from '../../../../assets/constants';
 
 export default StyleSheet.create({
+  tosmodal: {
+    flex: 1,
+    backgroundColor: COLORS.FADEDBLACK,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tosmodalcontainer: {
+    backgroundColor: COLORS.AKCRUBACKGROUND,
+    width: SIZES.ScreenWidth * 0.8,
+    height: SIZES.ScreenHeight * 0.8,
+    paddingHorizontal: 10,
+  },
+  tostitle: {
+    ...FONTS.Title2,
+    textAlign: 'center',
+    marginVertical: 10,
+  },
+  tosparagraph: {
+    ...FONTS.Title2White,
+    fontSize: 12,
+    paddingBottom: 10,
+    textAlign: 'center',
+  },
+
+  datePicker: {
+    width: SIZES.ScreenWidth / 2,
+    height: 50,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: COLORS.LIGHTGREY,
+    borderRadius: 5,
+    color: COLORS.LIGHTGREY,
+    fontSize: 16,
+    marginBottom: 10,
+  },
   bgimage: {
     height: SIZES.ScreenHeight,
     // width: SIZES.ScreenWidth,
@@ -10,8 +45,6 @@ export default StyleSheet.create({
     flex: 1,
     marginTop: SIZES.ScreenHeight * 0.09,
     marginHorizontal: SIZES.ScreenWidth * 0.03,
-   
-
   },
   mastercontainer: {
     height: SIZES.ScreenHeight,
