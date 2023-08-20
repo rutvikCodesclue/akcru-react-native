97d73afe59879a635171bd32028e86a4d445bf2a
Kwasi Fraser <followthescript@gmail.com>
2023-08-19 20:16:05 -0400

    Homepage connected

--- src/components/LargeListCategories/index.tsx
+++ src/components/LargeListCategories/index.tsx
@@ -6,28 +6,14 @@ import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {ClientStackParams} from '../../navigation/ClientStack';
 import React from 'react';
+import { IMovie } from '../../../types';
 
 interface LargeListCategoriesProps {
   Akcru_Content: {
-    id: string;
-    title: string;
-    movies: {
-      name: string;
-      desc: string;
-      actors: string[];
-      directors: string[];
-      genre: string[];
-      portrait_poster: string;
-      landscape_poster: string;
-      rating: number;
-      year: number;
-      rated: string;
-      length: string;
-      id: string;
-      movie_url: string;
-      youtubetrailer: string;
-    }[];
-  };
+        id: string;
+        title: string;
+        movies: IMovie[]; // Update this to match the IMovie structure
+    };
 }
 
 const LargeListCategories = (props: LargeListCategoriesProps) => {
@@ -35,32 +21,29 @@ const LargeListCategories = (props: LargeListCategoriesProps) => {
     useNavigation<NativeStackNavigationProp<ClientStackParams>>();
   const {Akcru_Content} = props;
   return (
-    <>
-      <Text style={{...FONTS.Title2}}>{Akcru_Content.title}</Text>
-      <FlatList
-        data={Akcru_Content.movies}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-        renderItem={({item}) => (
-          <View>
-            <TouchableOpacity
-              onPress={() => {
-                console.log('id:', item.id);
-                console.log('movie:', item.name);
-                navigation.navigate('ContentDetailScreen', {
-                  id: item.id,
-                  movie: item.id,
-                });
-              }}>
-              <Image
-                source={{uri: item.landscape_poster}}
-                style={styles.poster}
-              />
-            </TouchableOpacity>
-          </View>
-        )}
-      />
-    </>
+      <>
+          <Text style={{...FONTS.Title2, marginTop: 10}}>{Akcru_Content.title}</Text>
+          <FlatList
+              data={Akcru_Content.movies}
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+              renderItem={({item}) => (
+                  <View>
+                      <TouchableOpacity
+                          onPress={() => {
+                              console.log('id:', item.id);
+                              console.log('movie:', item.title);
+                              navigation.navigate('ContentDetailScreen', {
+                                  id: item.id,
+                                  movie: item.id,
+                              });
+                          }}>
+                          <Image source={{uri: item.landscapeURL}} style={styles.poster} />
+                      </TouchableOpacity>
+                  </View>
+              )}
+          />
+      </>
   );
 };
 
