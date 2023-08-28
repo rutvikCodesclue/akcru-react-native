f7026f12674ebcd49eee7b5a2b7da322ff6ee751
Obinna <obiknows88@gmail.com>
2023-08-28 15:33:49 -0400

    updated eventsPerSecond

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -17,7 +17,7 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   realtime: {
     log_level: 'debug', // FIXME: remove this for prod 
     params: {
-      eventsPerSecond: 5,
+      eventsPerSecond: 20,
     },
   },
   
