51fb63ef8a001d3adbbd9af5a213f4ce04fdbca8
Obinna <obiknows88@gmail.com>
2023-09-27 18:23:56 -0400

    more removal

--- src/screens/loginScreens/Signin/index.tsx
+++ src/screens/loginScreens/Signin/index.tsx
@@ -3,26 +3,18 @@ import {
   Text,
   ImageBackground,
   TouchableOpacity,
-  SafeAreaView,
-  Image,
-  ScrollView,
   Alert,
-  Modal
 } from 'react-native';
 import AkcruButtons from '../../../components/akcruButtons'
 import Inputs from '../../../components/input'
-import { COLORS, FONTS, SIZES } from '../../../../assets/constants'
+import { COLORS, FONTS } from '../../../../assets/constants'
 import React, {useState, useEffect} from 'react';
 import imageindex from '../../../../assets/images/imageindex';
 import styles from './styles';
-import {useFocusEffect, useNavigation} from '@react-navigation/native';
+import { useNavigation} from '@react-navigation/native';
 import { AuthStackParams } from '../../../navigation/AuthNavigation';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import {Icon} from '@rneui/themed';
-import { AkcruLogo, Applelogo, Googlelogo, Fblogo } from '../../../../assets/svg';
-import { supabase } from "../../../../lib/supabase";
-import { NoBottomTabStackParams } from '../../../navigation/NoBottomTabStack';
-import { API } from '../../../clients/api.client';
+import { AkcruLogo } from '../../../../assets/svg';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import useAuthStore from '../../../stores/auth.store';
 
@@ -30,24 +22,6 @@ import useAuthStore from '../../../stores/auth.store';
 const Signin = () => {
     const authStore = useAuthStore();
     const navigation = useNavigation<NativeStackNavigationProp<AuthStackParams>>();
-
-    useFocusEffect(
-        React.useCallback(() => {
-            // This code will run when the screen comes into focus (e.g., when navigating to this screen)
-            console.log('Signin focused [SigninScreen]');
-            console.log(
-                'Is logged in w/ Email/Password:',
-                email,
-                password,
-                // userName,
-            );
-
-            return () => {
-                // This code will run when the screen goes out of focus (e.g., when navigating away from this screen)
-                console.log('Exiting Signin unfocused [ExitSigninScreen]');
-            };
-        }, []),
-    );
 
     const showErrorAlert = () => {
         Alert.alert('Login error', 'Please try again.', [
