3f131054795c79a2b749bf7c588be95c699f5fda
Kwasi Fraser <followthescript@gmail.com>
2023-10-08 23:44:08 -0400

    Delete Cru member fixed

--- src/lib/api/cru.lib.ts
+++ src/lib/api/cru.lib.ts
@@ -31,7 +31,7 @@ export const updateCRUInfo = async (params: { name: string }) : Promise<ICru | u
 export const removeAUserFromCRU = async (userId: string) : Promise<ICru | undefined> => {
     try {
         // DELETE /v1/cru/me/remove
-        const { data } = await API.delete(`/v1/cru/me`, { data: { userId } });
+        const { data } = await API.delete(`/v1/cru/me/remove`, { data: { userId } });
         return data.CRU;
     } catch (error) {
         console.error(error);
