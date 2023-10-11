0528136e27b7b4054fa56cf6e991cf1cff403a9d
Obinna <obiknows88@gmail.com>
2023-10-11 14:36:03 -0400

    let users upload any kind of image (this could break the app if users upload bad images)

--- src/lib/api/user.lib.ts
+++ src/lib/api/user.lib.ts
@@ -1,62 +1,75 @@
-import { IUserProfile } from "../../../types";
-import { API } from "../../clients/api.client";
+import {IUserProfile} from '../../../types';
+import {API} from '../../clients/api.client';
 
-export const getMe = async () : Promise<IUserProfile | undefined> => {
+export const getMe = async (): Promise<IUserProfile | undefined> => {
     try {
         // GET /v1/auth/me
-        const { data } = await API.get(`/v1/auth/me`);
-    
+        const {data} = await API.get(`/v1/auth/me`);
+
         if (data.success === false) {
-            return undefined
+            return undefined;
         }
-        
+
         return data.user;
     } catch (error) {
         console.error(error);
         return undefined;
     }
-}
+};
 
-export const findAUser = async (params: { id?: string, username?: string, email?: string }) : Promise<IUserProfile | undefined> => {
+export const findAUser = async (params: {
+    id?: string;
+    username?: string;
+    email?: string;
+}): Promise<IUserProfile | undefined> => {
     try {
-        const { id, username, email } = params
+        const {id, username, email} = params;
         // GET /v1/cru/me
-        const { data } = await API.post(`/v1/user/find`, {
+        const {data} = await API.post(`/v1/user/find`, {
             id: id ?? undefined,
             username: username ?? undefined,
-            email: email ?? undefined
+            email: email ?? undefined,
         });
-    
+
         if (data.success === false) {
-            return undefined
+            return undefined;
         }
-        
+
         return data.user;
     } catch (error) {
         console.error(error);
         return undefined;
     }
-}
+};
 
-export const searchForUsers = async (search: string) : Promise<IUserProfile[] | []> => {
+export const searchForUsers = async (search: string): Promise<IUserProfile[] | []> => {
     try {
         // GET /v1/user/search
-        const { data } = await API.post(`/v1/user/search`, {
-            search
+        const {data} = await API.post(`/v1/user/search`, {
+            search,
         });
-    
+
         if (data.success === false) {
-            return []
+            return [];
         }
-    
+
         return data.result;
     } catch (error) {
         console.error(error);
         return [];
     }
-}
+};
 
-export const updateUser = async (params: { username?: string, firstName?: string, lastName?: string, email?: string, description?: string, phone?: string, password?: string, dob?: string }) : Promise<IUserProfile | undefined> => {
+export const updateUser = async (params: {
+    username?: string;
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    description?: string;
+    phone?: string;
+    password?: string;
+    dob?: string;
+}): Promise<IUserProfile | undefined> => {
     try {
         const {username, firstName, lastName, email, description, phone, password, dob} = params;
         // PUT /v1/user/
@@ -85,64 +98,69 @@ export const updateUser = async (params: { username?: string, firstName?: string
         console.error(error);
         return undefined;
     }
-}
+};
 
-export const updateUserProfilePicture = async (params: { uri: string, type: string, name: string }) : Promise<IUserProfile | undefined> => {
+export const updateUserProfilePicture = async (params: {
+    uri: string;
+    type: string;
+    name: string;
+}): Promise<IUserProfile | undefined> => {
     try {
         // this should be a file object
-        const { uri, type, name } = params
+        const {uri, type, name} = params;
 
         // type must be one of the following: image/jpeg, image/png, image/jpg
-        if (type !== 'image/jpeg' && type !== 'image/png') {
-            console.log("type must be one of the following: image/jpeg, image/png:", type);
-            return undefined
-        }
+        // if (type !== 'image/jpeg' && type !== 'image/png') {
+        //     console.log("type must be one of the following: image/jpeg, image/png:", type);
+        //     return undefined
+        // }
 
         const form = new FormData();
         form.append('image', {
             type, // Adjust the type as needed (e.g. png, jpeg, gif, etc)
             uri,
-            name,  // Adjust the filename as needed
+            name, // Adjust the filename as needed
         });
 
         // PUT /v1/user/profilePicture
-        const { data } = await API.put(`/v1/user/profilePicture`, form,
-        {
+        const {data} = await API.put(`/v1/user/profilePicture`, form, {
             headers: {
-                'Content-Type': 'multipart/form-data'
-            }
+                'Content-Type': 'multipart/form-data',
+            },
         });
 
-        console.log("data from profilePicture update:", data);
-        
-    
+        console.log('data from profilePicture update:', data);
+
         if (data.success === false) {
-            return undefined
+            return undefined;
         }
-        
+
         return data.updatedUser;
     } catch (error) {
         console.error(error);
         return undefined;
     }
-}
+};
 
-export const updateUserWatchTime = async (params: { watchTime?: number, movieId?: string,  }) : Promise<boolean | undefined> => {
+export const updateUserWatchTime = async (params: {
+    watchTime?: number;
+    movieId?: string;
+}): Promise<boolean | undefined> => {
     try {
         // TODO: in the future, we will want to track these things: watchTime, movieId, movieTime, etc...
-        const { watchTime, movieId } = params
+        const {watchTime, movieId} = params;
         // PUT /v1/watchtime/me
-        const { data } = await API.put(`/v1/watchtime/me`, {
+        const {data} = await API.put(`/v1/watchtime/me`, {
             // watchTime
         });
-    
+
         if (data.success === false) {
-            return false
+            return false;
         }
-        
+
         return true;
     } catch (error) {
         console.error(error);
         return undefined;
     }
-}
+};
