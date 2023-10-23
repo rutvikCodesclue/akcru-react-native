b1fa29de91ec5cecc9bf62cbf5e4cdadc4418cad
Kwasi Fraser <followthescript@gmail.com>
2023-10-22 22:56:22 -0400

    Cosmetics-

--- src/screens/loginScreens/Onboarding/OnBoard3.tsx
+++ src/screens/loginScreens/Onboarding/OnBoard3.tsx
@@ -10,6 +10,8 @@ import {
     Alert,
     TextInput,
     FlatList,
+    Modal,
+    Image,
 } from 'react-native';
 import React, {useState, useEffect} from 'react';
 import {COLORS, FONTS, SIZES} from '../../../../assets/constants';
@@ -33,6 +35,8 @@ const OnBoard3 = () => {
     const navigation = useNavigation<NativeStackNavigationProp<AuthStackParams>>();
 
     const [checkedGenres, setCheckedGenres] = useState<Record<string, boolean>>({});
+
+    const [archetypeModal, setArchetypeModal]= useState(false)
     
     const handleCheckboxChange = (genreId: string) => {
         // Check if the genre is already selected
@@ -57,6 +61,11 @@ const OnBoard3 = () => {
         }
     };
 
+    const [archetypeKey, setArchetypeKey] = useState('');
+    const [archetypeName, setArchetypeName] = useState('');
+    const [archetypeImage, setArchetypeImage] = useState<string | null>(null);
+    const [archetypeDescription, setArchetypeDescription] = useState('');
+
     const handleFinishButton = () => {
         const selectedGenres = Object.keys(checkedGenres).filter(genreId => checkedGenres[genreId]);
 
@@ -68,16 +77,28 @@ const OnBoard3 = () => {
                 return genreObject ? genreObject.genre : '';
             });
 
-            const archetypeKey = genreNames.sort().join(', ');
+            const newArchetypeKey = genreNames.sort().join(', ');
 
-            console.log('Archetype Key:', archetypeKey);
+            console.log('Archetype Key:', newArchetypeKey);
 
-            const selectedArchetype = archetypeMapping[archetypeKey];
+            setArchetypeKey(newArchetypeKey);
+
+            const selectedArchetype = archetypeMapping[newArchetypeKey];
 
             if (selectedArchetype) {
-                console.log('Selected Archetype:', selectedArchetype);
-                // You can also navigate or perform any other action here
-                navigation.navigate('NoBottomStack', {screen: 'ContentSwipe'});
+                setArchetypeModal(true);
+                const newArchetypeName = selectedArchetype.name;
+                const newArchetypeImage = selectedArchetype.image; // Set the image here
+                const newArchetypeDescription = selectedArchetype.description; // Set the description here
+                console.log('Selected Archetype:', newArchetypeName);
+                setArchetypeName(newArchetypeName);
+                setArchetypeImage(newArchetypeImage);
+                setArchetypeDescription(newArchetypeDescription);
+
+                setTimeout(() => {
+                    setArchetypeModal(false); // Hide the archetype modal after 8 seconds
+                    navigation.navigate('NoBottomStack', {screen: 'ContentSwipe'});
+                }, 8000); // 8 seconds (8000 milliseconds)
             } else {
                 console.log('No matching archetype found for the selected genres.');
             }
@@ -85,8 +106,6 @@ const OnBoard3 = () => {
             console.log('Please select exactly 2 genres.');
         }
     };
-
-
    
 
     const filteredGenres = MOVIE_GENRES.filter(genre => genre.id !== '0');
@@ -96,7 +115,7 @@ const OnBoard3 = () => {
     return (
         <View>
             <ImageBackground style={styles.bgimage} source={imageindex.AkcruonboardBG} resizeMode={'cover'}>
-                <KeyboardAvoidingView behavior="padding" style={{flex: 1, marginBottom: 50}}>
+                <View style={{flex: 1, marginBottom: 50}}>
                     <View style={styles.container}>
                         <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
                             <TouchableOpacity onPress={() => navigation.pop()} style={styles.backbutton}>
@@ -163,7 +182,6 @@ const OnBoard3 = () => {
                                 )}
                             />
                         </View>
-
                     </View>
                     <View>
                         <View style={{alignItems: 'center'}}>
@@ -175,7 +193,52 @@ const OnBoard3 = () => {
                             />
                         </View>
                     </View>
-                </KeyboardAvoidingView>
+                </View>
+                {/* Archetype Modal */}
+                <Modal animationType="fade" transparent={true} visible={archetypeModal}>
+                    <View
+                        style={{
+                            flex: 1,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            backgroundColor: 'rgba(0, 0, 0, 0.9)',
+                            
+                        }}>
+                        <Text style={{...FONTS.Title1}}>Your Archetype is:</Text>
+
+                        {archetypeName && (
+                            <Text
+                                style={{
+                                    ...FONTS.Title3,
+                                    textAlign: 'center',
+                                    marginVertical: 10,
+                                    color: COLORS.PURPLE,
+                                }}>
+                                "{archetypeName}"
+                            </Text>
+                        )}
+                        <View>
+                            {archetypeImage && (
+                                <Image
+                                    source={{uri: archetypeImage}}
+                                    style={{
+                                        width: SIZES.ScreenWidth / 1.2,
+                                        height: SIZES.ScreenWidth / 1.2,
+                                        borderRadius: 5,
+                                        alignSelf: 'center',
+                                    }}
+                                />
+                            )}
+                        </View>
+
+                        {archetypeDescription && (
+                            <Text style={{...FONTS.paragraph1, textAlign: 'center', marginVertical: 10, marginHorizontal: 15, color: COLORS.MIDORANGE}}>
+                                {archetypeDescription}
+                            </Text>
+                        )}
+
+                    </View>
+                </Modal>
             </ImageBackground>
         </View>
     );
