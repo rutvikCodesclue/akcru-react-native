5ef106f58f44be91ec5ab3a77b05c0187be003c4
Kwasi Fraser <followthescript@gmail.com>
2023-10-25 22:41:10 -0400

    SendAD functionality frontend-

---
+++ src/components/UserWalletSearchCard/index.tsx
@@ -0,0 +1,140 @@
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import React, { useState } from 'react';
+import {Avatar, Icon} from '@rneui/base';
+import {SIZES, FONTS, COLORS} from '../../../assets/constants';
+import AkcruLevels from '../akcruBadges';
+import { FAKE_USER_PROFILES } from '../../../assets/constants/Mockusers';
+import LinearGradient from 'react-native-linear-gradient';
+import { selectAvatarBorderColor } from '../../util/util';
+import { IUserProfile } from '../../../types';
+import imageindex from '../../../assets/images/imageindex';
+
+const MAX_USERDESC_LENGTH = 50; // Maximum number of characters for the userDesc
+
+type UserWalletSearchCardProps = {
+    userPicture?: string;
+    userName: string;
+    influencer?: boolean;
+    akcruBadge: any;
+    onPress: () => void;
+    onPressIn: () => void;
+    userID: any;
+    userDesc?: string;
+};
+
+const UserSearchCard = ({
+  userPicture,
+  userName,
+  influencer,
+  akcruBadge,
+  onPress,
+  onPressIn,
+  userID,
+  userDesc,
+}: UserWalletSearchCardProps) => {
+  const truncateduserDesc =
+    userDesc && userDesc.length > MAX_USERDESC_LENGTH
+      ? userDesc.slice(0, MAX_USERDESC_LENGTH) + '...'
+      : userDesc;
+
+
+      
+
+  return (
+      <View
+          style={{
+              borderRadius: 5,
+              backgroundColor: COLORS.TAGCOLOR,
+              width: SIZES.ScreenWidth,
+              height: SIZES.ScreenHeight / 9.3,
+          }}>
+          <LinearGradient
+              // Background Linear Gradient
+              colors={[COLORS.FADEDBLACK, 'transparent', COLORS.FADEDBLACK]}
+              style={{
+                  position: 'absolute',
+                  left: 0,
+                  right: 0,
+                  top: 0,
+                  width: SIZES.ScreenWidth,
+                  borderRadius: 5,
+                  height: SIZES.ScreenHeight / 9.3,
+              }}
+          />
+          <View style={{padding: 10}}>
+              <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                  <View style={{flexDirection: 'row'}}>
+                      <View style={{marginRight: 8}}>
+                          <TouchableOpacity onPress={onPress}>
+                              <Avatar
+                                  rounded
+                                  size={40}
+                                  source={
+                                      userPicture
+                                          ? {
+                                                uri: userPicture,
+                                            }
+                                          : imageindex.Akcruplaceholder
+                                  }
+                                  avatarStyle={{
+                                      borderWidth: 2,
+                                      borderColor: selectAvatarBorderColor(akcruBadge),
+                                  }}
+                              />
+                          </TouchableOpacity>
+                      </View>
+                      <View>
+                          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                              <Text style={{...FONTS.Title2}}>{userName}</Text>
+                              {influencer && (
+                                  <Icon
+                                      name="ribbon"
+                                      type="ionicon"
+                                      color={COLORS.AKCRUBLUE}
+                                      size={20}
+                                      style={{marginLeft: 5}}
+                                  />
+                              )}
+                          </View>
+
+                          {akcruBadge === 'AKCRUIT' && (
+                              <View>
+                                  <AkcruLevels.AkcruBadgeAkcruit />
+                              </View>
+                          )}
+                          {akcruBadge === 'GUARDIAN' && (
+                              <View>
+                                  <AkcruLevels.AkcruBadgeGuardian />
+                              </View>
+                          )}
+                          {akcruBadge === 'HERO' && (
+                              <View>
+                                  <AkcruLevels.AkcruBadgeHero />
+                              </View>
+                          )}
+                          {akcruBadge === 'SUPERHERO' && (
+                              <View>
+                                  <AkcruLevels.AkcruBadgeSuperHero />
+                              </View>
+                          )}
+                      </View>
+                  </View>
+                  <TouchableOpacity onPress={onPressIn}>
+                    <View>
+                      <Text style={{...FONTS.Title1, marginRight: 40, color: COLORS.AKCRUBLUE}}>SEND</Text>
+                  </View>
+                  </TouchableOpacity>
+                  
+              </View>
+
+              <View>
+                  <Text style={{...FONTS.paragraph1, fontSize: 12}}>{truncateduserDesc}</Text>
+              </View>
+          </View>
+      </View>
+  );
+};
+
+export default UserSearchCard;
+
+const styles = StyleSheet.create({});
