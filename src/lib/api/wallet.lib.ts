fb570d0503007244d071347accd62a1fc733a92d
Obinna <obiknows88@gmail.com>
2023-10-28 10:52:19 -0400

    added/tested purchase mit functionality

--- src/lib/api/wallet.lib.ts
+++ src/lib/api/wallet.lib.ts
@@ -1,17 +1,35 @@
-import { API } from "../../clients/api.client";
+import {API} from '../../clients/api.client';
 
-export const getTotalSupplyOfAD = async () : Promise<Number | undefined> => {
+export const getTotalSupplyOfAD = async (): Promise<Number | undefined> => {
     try {
         // GET /v1/wallet/total-supply
-        const { data } = await API.get(`/v1/wallet/total-supply`);
-    
+        const {data} = await API.get(`/v1/wallet/total-supply`);
+
         if (data.success === false) {
-            return undefined
+            return undefined;
         }
-        
+
         return data.totalSupply;
     } catch (error) {
         console.error(error);
         return undefined;
     }
-}
+};
+
+export const purchaseMIT = async (params: {amount: number}): Promise<boolean> => {
+    try {
+        // POST /v1/wallet/purchase/mit
+        const {data} = await API.post(`/v1/wallet/purchase/mit`, {
+            amount: params.amount,
+        });
+
+        if (data.success === false) {
+            return false;
+        }
+
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+};
