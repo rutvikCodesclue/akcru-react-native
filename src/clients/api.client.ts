1c8e60045078b7bbb7ac5ffb8c2ddb9e708283c7
Ahmed Tahir Shekhani <ahmedtahir2000@gmail.com>
2024-03-12 02:30:12 +0500

    castle io integrated

--- src/clients/api.client.ts
+++ src/clients/api.client.ts
@@ -3,10 +3,21 @@ export const isProduction = process.env.NODE_ENV === 'production';
 import {DEV_API_URL} from '@env';
 import authStore from '../stores/auth.store';
 
+
 // console.log('Current ENV for API:', process.env.NODE_ENV);
 // console.log('Current ENV for API:', DEV_API_URL);
-// console.log('DEV_API_URL:', DEV_API_URL);
+console.log('DEV_API_URL:', DEV_API_URL);
 // console.log('NODE_ENV:', process.env.NODE_ENV);
+
+import Castle from "@castleio/react-native-castle";
+
+
+const addRequestTokenHeader = async () => {
+    const requestTokenHeaderName = await Castle.requestTokenHeaderName();
+const requestToken = await Castle.createRequestToken();
+return  requestToken
+
+}
 
 const determineBaseURL = (): string => {
     switch (process.env.NODE_ENV) {
@@ -18,6 +29,8 @@ const determineBaseURL = (): string => {
             return DEV_API_URL ?? 'http://10.0.2.2:3000';
     }
 };
+console.log('Backend API Client Base URL:', determineBaseURL());
+
 
 const API = axios.create({
     baseURL: determineBaseURL(),
@@ -30,12 +43,22 @@ const API = axios.create({
     },
 });
 
+
 API.interceptors.request.use(
     async config => {
         const session = authStore.getState().getSession();
 
         if (session) {
             config.headers.Authorization = `Bearer ${session.access_token}`;
+        }
+
+        if (config.method?.toLowerCase() === 'post') {
+            // Adjust this part according to your actual data structure
+            const castle_request_token = await addRequestTokenHeader()
+            config.data = {
+                ...config.data,
+                castle_request_token: castle_request_token,
+            };
         }
 
         return config;
