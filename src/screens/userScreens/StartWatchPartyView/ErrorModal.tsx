b3e59d86aa0df441c27ba16f838704908c1a6945
Ahmed Tahir Shekhani <ahmedtahir2000@gmail.com>
2024-03-24 19:30:17 +0500

    added realtime permission muteall

---
+++ src/screens/userScreens/StartWatchPartyView/ErrorModal.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Modal, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { COLORS, FONTS } from '../../../../assets/constants';
+
+const ErrorModal = ({ errorMessage, onClose }) => {
+    return (
+        <Modal animationType="fade" transparent={true} visible={true}>
+            <View style={styles.modalContainer}>
+                <View style={styles.modalContent}>
+                    <Text style={styles.errorMessage}>{errorMessage}</Text>
+                    <TouchableOpacity onPress={onClose} style={styles.okButton}>
+                        <Text style={styles.okButtonText}>OK</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </Modal>
+    );
+};
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+        backgroundColor: COLORS.AKCRUBACKGROUND,
+        padding: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    errorMessage: {
+        ...FONTS.Title3,
+        marginBottom: 20,
+        textAlign: 'center',
+    },
+    okButton: {
+        backgroundColor: 'red', // Adjust color as needed
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+    },
+    okButtonText: {
+        ...FONTS.Title3,
+        color: COLORS.WHITE, // Adjust color as needed
+    },
+});
+
+export default ErrorModal;
