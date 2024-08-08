9250c4fec3b3677aba90e901538a62aa1c485cbc
TahaFazlani <tahafazlani79@gmail.com>
2024-08-09 01:50:20 +0500

    add akcru Opener

--- src/screens/loginScreens/Welcome/index.tsx
+++ src/screens/loginScreens/Welcome/index.tsx
@@ -14,89 +14,46 @@ import {appVersion} from '../../../../assets/constants/Data';
 import {Platform} from 'react-native';
 import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
 import LinearGradient from 'react-native-linear-gradient';
+import AkcruAppOpener from '../../../components/AkcruAppOpener';
 
 const Welcome = () => {
+    const navigation = useNavigation<NativeStackNavigationProp<AuthStackParams>>();
+    const authStore = useAuthStore();
+
+    const [showLoginError, setShowLoginError] = useState(false);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+    const [showOpener, setShowOpener] = useState<boolean>(true);
+
     useEffect(() => {
         const _checkPermissions = async () => {
             if (Platform.OS === 'android') {
-                const audioResult = await check(PERMISSIONS.ANDROID.RECORD_AUDIO);
-                if (audioResult !== RESULTS.GRANTED) {
-                    const audioRequestResult = await request(PERMISSIONS.ANDROID.RECORD_AUDIO);
-                    if (audioRequestResult === RESULTS.GRANTED) {
-                    }
-                }
+                const permissions = [
+                    PERMISSIONS.ANDROID.RECORD_AUDIO,
+                    PERMISSIONS.ANDROID.READ_CONTACTS,
+                    PERMISSIONS.ANDROID.CAMERA,
+                    PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE
+                ];
 
-                const contactResult = await check(PERMISSIONS.ANDROID.READ_CONTACTS);
-                if (contactResult !== RESULTS.GRANTED) {
-                    const contactResult = await request(PERMISSIONS.ANDROID.READ_CONTACTS);
-                    if (contactResult === RESULTS.GRANTED) {
-                        console.log('contact permission granted');
-                        console.log('contactResult =>', contactResult);
-                    }
-                }
-
-                const cameraResult = await check(PERMISSIONS.ANDROID.CAMERA);
-                if (cameraResult !== RESULTS.GRANTED) {
-                    const cameraRequestResult = await request(PERMISSIONS.ANDROID.CAMERA);
-                    if (cameraRequestResult === RESULTS.GRANTED) {
-                    }
-                }
-
-                const audioMediaResult = await check(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
-                if (audioMediaResult !== RESULTS.GRANTED) {
-                    const audioMediaRequestResult = await request(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
-                    if (audioMediaRequestResult === RESULTS.GRANTED) {
-                    }
-                }
-
-                const imagesMediaResult = await check(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
-                if (imagesMediaResult !== RESULTS.GRANTED) {
-                    const imagesMediaRequestResult = await request(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
-                    if (imagesMediaRequestResult === RESULTS.GRANTED) {
-                    }
-                }
-
-                const videoMediaResult = await check(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
-                if (videoMediaResult !== RESULTS.GRANTED) {
-                    const videoMediaRequestResult = await request(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
-                    if (videoMediaRequestResult === RESULTS.GRANTED) {
+                for (let permission of permissions) {
+                    const result = await check(permission);
+                    if (result !== RESULTS.GRANTED) {
+                        await request(permission);
                     }
                 }
             }
 
             if (Platform.OS === 'ios') {
-                const cameraResult = await check(PERMISSIONS.IOS.CAMERA);
-                if (cameraResult !== RESULTS.GRANTED) {
-                    const cameraRequestResult = await request(PERMISSIONS.IOS.CAMERA);
-                    if (cameraRequestResult === RESULTS.GRANTED) {
-                    }
-                }
+                const permissions = [
+                    PERMISSIONS.IOS.CAMERA,
+                    PERMISSIONS.IOS.MICROPHONE,
+                    PERMISSIONS.IOS.MEDIA_LIBRARY
+                ];
 
-                const micResult = await check(PERMISSIONS.IOS.MICROPHONE);
-                if (micResult !== RESULTS.GRANTED) {
-                    const micRequestResult = await request(PERMISSIONS.IOS.MICROPHONE);
-                    if (micRequestResult === RESULTS.GRANTED) {
-                    }
-                }
-
-                const audioMediaResult = await check(PERMISSIONS.IOS.MEDIA_LIBRARY);
-                if (audioMediaResult !== RESULTS.GRANTED) {
-                    const audioMediaRequestResult = await request(PERMISSIONS.IOS.MEDIA_LIBRARY);
-                    if (audioMediaRequestResult === RESULTS.GRANTED) {
-                    }
-                }
-
-                const imagesMediaResult = await check(PERMISSIONS.IOS.MEDIA_LIBRARY);
-                if (imagesMediaResult !== RESULTS.GRANTED) {
-                    const imagesMediaRequestResult = await request(PERMISSIONS.IOS.MEDIA_LIBRARY);
-                    if (imagesMediaRequestResult === RESULTS.GRANTED) {
-                    }
-                }
-
-                const videoMediaResult = await check(PERMISSIONS.IOS.MEDIA_LIBRARY);
-                if (videoMediaResult !== RESULTS.GRANTED) {
-                    const videoMediaRequestResult = await request(PERMISSIONS.IOS.MEDIA_LIBRARY);
-                    if (videoMediaRequestResult === RESULTS.GRANTED) {
+                for (let permission of permissions) {
+                    const result = await check(permission);
+                    if (result !== RESULTS.GRANTED) {
+                        await request(permission);
                     }
                 }
             }
@@ -105,37 +62,57 @@ const Welcome = () => {
         _checkPermissions();
     }, []);
 
-    const authStore = useAuthStore();
-
-    const navigation = useNavigation<NativeStackNavigationProp<AuthStackParams>>();
-
-    const [showLoginError, setShowLoginError] = useState(false);
-
-    const [loading, setLoading] = useState<boolean>(true);
-
-    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-
     useEffect(() => {
         const checkAuth = async () => {
-            await authStore.hydrateAuth();
-            const accessToken = await AsyncStorage.getItem('access_token');
-            const isAuthed = authStore.getUser() !== null && authStore.getSession() !== null;
-            const isLoggedInWithToken = isAuthed && accessToken !== null;
-            if (isLoggedInWithToken) {
-                navigation.navigate('NoBottomStack', {screen: 'ClientTabNavigator'});
-            }
-            setIsLoggedIn(isLoggedInWithToken);
+          await authStore.hydrateAuth();
+          const accessToken = await AsyncStorage.getItem('access_token');
+          const isAuthed = authStore.getUser()!== null && authStore.getSession()!== null;
+          const isLoggedInWithToken = isAuthed && accessToken!== null;
+      
+          if (isLoggedInWithToken) {
+            await new Promise(resolve => {
+              const onAnimationFinish = () => {
+                resolve();
+              };
+              setShowOpener(true);
+              setTimeout(onAnimationFinish, 3400);
+            });
+            navigation.navigate('NoBottomStack', { screen: 'ClientTabNavigator' });
+          } else {
+            await new Promise(resolve => {
+              const onAnimationFinish = () => {
+                resolve();
+              };
+              setShowOpener(true);
+              setTimeout(onAnimationFinish, 3400);
+            });
+            navigation.navigate('NoBottomStack', { screen: 'Signin' });
+          }
         };
         checkAuth().catch(err => {
-            console.error('Error checking auth', err);
+          console.error('Error checking auth', err);
+          setLoading(false);
+          setShowOpener(false);
         });
-    }, []);
+      }, []);
+
+    
 
     async function handleLogout() {
         await AsyncStorage.removeItem('access_token');
         await authStore.logout();
         navigation.navigate('Signin');
         setIsLoggedIn(false);
+    }
+
+    if (loading) {
+        return (
+            <View>
+                {showOpener && (
+                    <AkcruAppOpener onAnimationFinish={() => setShowOpener(false)} />
+                )}
+            </View>
+        );
     }
 
     return (
@@ -152,7 +129,35 @@ const Welcome = () => {
                     }}
                 />
                 <View style={styles.container}>
-                    {isLoggedIn ? (
+                    <View>
+                        <View style={{ alignItems: 'center' }}>
+                            <AkcruLogo width={200} height={60} />
+                        </View>
+                        <View style={{ flex: 1, justifyContent: 'flex-end', marginBottom: 50 }}>
+                            <View style={{ marginBottom: '15%' }}>
+                                <View style={{ marginVertical: 15 }}>
+                                    <AkcruButtons.LrgButton
+                                        color={COLORS.AKCRUBLUE}
+                                        btnname={'Sign in'}
+                                        onPress={() => navigation.navigate('Signin')}
+                                        disabled={false}
+                                    />
+                                </View>
+                                <View>
+                                    <AkcruButtons.LrgButton
+                                        color={COLORS.PURPLE}
+                                        btnname={'Create Account'}
+                                        onPress={() => navigation.navigate('OnboardEmail')}
+                                        disabled={false}
+                                    />
+                                </View>
+                            </View>
+                            <Text style={{ ...FONTS.Title2White, textAlign: 'center' }}>
+                                version {appVersion[0].version}
+                            </Text>
+                        </View>
+                    </View>
+                    {/* {isLoggedIn ? (
                         <View style={styles.container2}>
                             <AkcruLogo width={200} height={60} />
                             <Text style={{...FONTS.Title1, paddingBottom: 10}}>{`Welcome back, ${
@@ -228,7 +233,7 @@ const Welcome = () => {
                                 </Text>
                             </View>
                         </View>
-                    )}
+                    )} */}
                     <Modal animationType="fade" transparent={true} visible={showLoginError}>
                         <View
                             style={{
@@ -236,7 +241,8 @@ const Welcome = () => {
                                 backgroundColor: 'rgba(0, 0, 0, 0.5)',
                                 justifyContent: 'center',
                                 alignItems: 'center',
-                            }}>
+                            }}
+                        >
                             <View
                                 style={{
                                     backgroundColor: COLORS.AKCRUBACKGROUND,
@@ -244,26 +250,13 @@ const Welcome = () => {
                                     borderRadius: 10,
                                     alignItems: 'center',
                                     marginHorizontal: 15,
-                                }}>
-                                <Text
-                                    style={{
-                                        ...FONTS.Title3,
-                                        marginBottom: 10,
-                                        textAlign: 'center',
-                                    }}>
+                                }}
+                            >
+                                <Text style={{ ...FONTS.Title3, marginBottom: 10, textAlign: 'center' }}>
                                     {'Login error, Please try again.'}
                                 </Text>
-                                <TouchableOpacity
-                                    onPress={() => {
-                                        setShowLoginError(false);
-                                    }}>
-                                    <Text
-                                        style={{
-                                            ...FONTS.Title2,
-                                            marginBottom: 10,
-                                            textAlign: 'center',
-                                            color: COLORS.MIDORANGE,
-                                        }}>
+                                <TouchableOpacity onPress={() => setShowLoginError(false)}>
+                                    <Text style={{ ...FONTS.Title2, marginBottom: 10, textAlign: 'center', color: COLORS.MIDORANGE }}>
                                         {'Close'}
                                     </Text>
                                 </TouchableOpacity>
