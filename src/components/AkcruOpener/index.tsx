2ab4f24ac2a5b6cc5dd1928030698e0ac03ea170
Kwasi Fraser <followthescript@gmail.com>
2025-07-06 19:20:28 -0400

    added the new akcru opener

--- src/components/AkcruOpener/index.tsx
+++ src/components/AkcruOpener/index.tsx
@@ -1,7 +1,6 @@
 import {StyleSheet, View} from 'react-native';
 import React from 'react';
-import {COLORS, SIZES} from '../../../assets/constants/theme';
-import LottieView from 'lottie-react-native';
+import {COLORS} from '../../../assets/constants/theme';
 import Video from 'react-native-video';
 
 type AkcruOpenerProps = {
@@ -11,13 +10,16 @@ type AkcruOpenerProps = {
 const AkcruOpener = ({onAnimationFinish}: AkcruOpenerProps) => {
     return (
         <View style={styles.activitycontainer}>
-            <Video source={require('../../../assets/sounds/akcrusound1.mp3')} repeat={false} />
-            <LottieView
-                source={require('../../../assets/lottie/Akcruopener1.json')}
-                autoPlay
-                loop={false}
-                style={{width: SIZES.ScreenHeight, height: SIZES.ScreenWidth}}
-                onAnimationFinish={onAnimationFinish}
+            <Video
+                style={{
+                    width: '100%',
+                    height: '100%',
+                }}
+                source={require('../../../assets/video/Akcru_NeonLogo_Audio-Final.mp4')}
+                repeat={false}
+                resizeMode="cover"
+                onEnd={onAnimationFinish}
+                onError={onAnimationFinish}
             />
         </View>
     );
