cad1884202711988c57c8dfbe5aacc8c520f9494
Ahmed Tahir Shekhani <ahmedtahir2000@gmail.com>
2025-08-17 20:00:45 +0200

    package updated

--- src/screens/contentScreens/PlayContentScreen/index.tsx
+++ src/screens/contentScreens/PlayContentScreen/index.tsx
@@ -223,9 +223,11 @@ export default function ContentPlayer({navigation}: Props) {
                                     videoRef={videoRef}
                                     source={{
                                         uri: movie.movieURL,
+                                        ad: {
+                                            adTagUrl: "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/vmap_ad_samples&sz=640x480&cust_params=sample_ar%3Dpremidpostoptimizedpodbumper&ciu_szs=300x250&gdfp_req=1&ad_rule=1&output=vmap&unviewed_position_start=1&env=vp&impl=s&cmsid=496&vid=short_onecue&correlator="
+                                        }
                                     }}
                                     resizeMode="cover"
-                                    posterResizeMode="cover"
                                     tapAnywhereToPause={false}
                                     preventsDisplaySleepDuringVideoPlayback={true}
                                     toggleResizeModeOnFullscreen={false}
